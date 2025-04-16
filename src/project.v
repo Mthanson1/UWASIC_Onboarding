e3c9d6831abcbeffc21f1b9e33a142af03dc7fed
Mathew Hanson <mthanson2004@gmail.com>
2025-04-16 15:39:47 -0400

    incorrect decloration of spi_registers in project.v

--- src/project.v
+++ src/project.v
@@ -19,11 +19,11 @@ module tt_um_uwasic_mhansonSPI (
 
   assign uio_oe = 8'hFF; // set all IOs to output
 
-  wire [7:0] = en_reg_out_7_0;
-  wire [7:0] = en_reg_out_15_8;
-  wire [7:0] = en_reg_pwm_7_0;
-  wire [7:0] = en_reg_pwm_15_8;
-  wire [7:0] = pwm_duty_cycle;
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
 
   pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
